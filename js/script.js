85bd005e35e0269d44c9bf9088391a2f7132e3cf
GB Factory <info@gbfactory.net>
2021-10-18 23:40:26 +0200

    Fix banner Play Store

--- js/script.js
+++ js/script.js
@@ -15,9 +15,10 @@ if (navigator.userAgent != 'EuganeoOrarioApp') {
 }
 
 // Mostra il tasto per scaricare l'app
-if (navigator.userAgent.toLowerCase().indexOf('android') > -1) {
-    $('#scarica-app').html('<a href="market://details?id=net.gbfactory.euganeoorario"><img src="./images/googleplay_it.png" alt="Disponibile su Google Play"></a>');
-}
+// ( ! ) disattivato fino a rilascio nuova applicazione react native!
+// if (navigator.userAgent.toLowerCase().indexOf('android') > -1) {
+//     $('#scarica-app').html('<a href="market://details?id=net.gbfactory.euganeoorario"><img src="./images/googleplay_it.png" alt="Disponibile su Google Play"></a>');
+// }
 
 // Navbar responsiva
 $(".navbar-burger").click(function() {
